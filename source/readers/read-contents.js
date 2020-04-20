fa658482d90ffdab47696f51abf30bfcca52dc81
Mármarosi József <marmarosi@logikum.hu>
2020-04-20 22:20:50 +0200

    Add omitting contents.

--- source/readers/read-contents.js
+++ source/readers/read-contents.js
@@ -29,25 +29,31 @@ function readContents( contentPath, submenuFile, filingCabinet, renderer ) {
 
     if (stats.isDirectory()) {
 
-      // Create new stores for the language.
-      var contentStock = filingCabinet.contents.create( item );
-      var menuStock = filingCabinet.menus.create( item );
+      if (item.endsWith( '--' )) {
+        // Skip this content.
+        logger.contentSkipped( item.substring( 0, item.length - 2 ) );
+      }
+       else {
+        // Create new stores for the language.
+        var contentStock = filingCabinet.contents.create( item );
+        var menuStock = filingCabinet.menus.create( item );
 
-      // Save the language for the menu.
-      filingCabinet.languages.push( item );
-      logger.localeFound( typeName, item );
+        // Save the language for the menu.
+        filingCabinet.languages.push( item );
+        logger.localeFound( typeName, item );
 
-      // Find and add contents for the language.
-      processContents(
-        itemPath,                   // content directory
-        '',                         // content root
-        submenuFile,                // submenu filename
-        contentStock,               // content stock
-        menuStock,                  // menu stock
-        filingCabinet.references,   // reference drawer
-        item,                       // language
-        renderer                    // markdown renderer
-      );
+        // Find and add contents for the language.
+        processContents(
+          itemPath,                   // content directory
+          '',                         // content root
+          submenuFile,                // submenu filename
+          contentStock,               // content stock
+          menuStock,                  // menu stock
+          filingCabinet.references,   // reference drawer
+          item,                       // language
+          renderer                    // markdown renderer
+        );
+      }
     } else
       // Unused file.
       logger.fileSkipped( typeName, itemPath );
